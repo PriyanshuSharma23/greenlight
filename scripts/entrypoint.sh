901cb7819f0616e145d40d76fa3395f6fb4cf8c5
Priyanshu Sharma <inbox.priyanshu@gmail.com>
2024-08-21 13:42:56 +0530

    move migrations to a seperate image

--- scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# Run database migrations
-echo "Running migrations..."
-migrate -path=/migrations-dir -database="$dbDsn" up
-
-# Start the API
-echo "Starting API..."
-exec /usr/local/bin/api -db-dsn="$dbDsn"
